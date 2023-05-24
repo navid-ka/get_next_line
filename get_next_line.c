f17397428aec894aadcbc8e105432da61b98edb8
bifrost <nkeyani-@student.42barcelona.com>
2023-05-24 16:37:33 +0200

    strjoin

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,18 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 13:21:10 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/24 13:31:22 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/05/24 15:55:28 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*get_next_line(int fd);
 
 int main()
 {
+
+	printf("%s", ft_strjoin("Hola ", "Que Tal"));
 	return (0);
 }
