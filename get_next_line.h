3b07e1a4bba40791dda4f5113596a4be92db76bd
bifrost <nkeyani-@student.42barcelona.com>
2023-05-24 15:52:49 +0200

    utils v1

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 13:09:40 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/24 13:30:40 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/05/24 15:52:05 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 # include <stdlib.h>
 
 size_t	ft_strlen(char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
 char	*get_next_line(int fd);
 
 #endif
