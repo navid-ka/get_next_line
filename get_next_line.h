f31da328f6a9142a836d92ef2d681aa14cace478
Your Name <you@example.com>
2023-05-29 19:02:25 +0200

    changing all

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
+/*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 13:09:40 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/28 11:58:22 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/05/29 14:43:24 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 100000
+#  define BUFFER_SIZE 4
 # endif
 
 size_t	ft_strlen(char *s);
