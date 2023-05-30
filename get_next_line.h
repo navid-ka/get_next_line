d88aa0f50280b12837c25a7f6dcce261c8b4195b
Tu Nombre <you@example.com>
2023-05-30 13:53:23 +0200

    gnl finished

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
+/*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 13:09:40 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/29 14:43:24 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/05/30 13:39:08 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@
 # endif
 
 size_t	ft_strlen(char *s);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(char *s);
-char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strchr(char *s, int c);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strjoin(char *s1, char *s2);
 char	*get_next_line(int fd);
 
 #endif
