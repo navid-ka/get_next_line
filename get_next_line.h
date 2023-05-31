b13cd6ec111eb79940c76dfc55ed644d0a98ff75
Your Name <you@example.com>
2023-05-31 18:09:12 +0200

    2 extra lines

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
+/*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 13:09:40 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/30 13:39:08 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/05/31 17:00:07 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@
 # endif
 
 size_t	ft_strlen(char *s);
-char	*ft_strchr(char *s, int c);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strchr(const char *str, int c);
 char	*ft_strjoin(char *s1, char *s2);
+char	*free_storage(char *storage);
 char	*get_next_line(int fd);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -3,14 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bifrost <nkeyani-@student.42barcelona.c    +#+  +:+       +#+        */
+/*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 13:09:31 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/05/31 00:10:25 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/05/31 17:00:20 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*free_storage(char *storage)
+{
+	free(storage);
+	return (NULL);
+}
 
 size_t	ft_strlen(char *s)
 {
@@ -33,13 +39,13 @@ char	*ft_strjoin(char *s1, char *s2)
 	if (!s1)
 	{
 		s1 = malloc(1 * sizeof(char));
+		if (!s1)
+			return (NULL);
 		s1[0] = '\0';
 	}
-	if (!s1 || !s2)
-		return (NULL);
 	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (str == NULL)
-		return (NULL);
+		return (free_storage(s1));
 	i = -1;
 	c = 0;
 	if (s1)
@@ -52,20 +58,22 @@ char	*ft_strjoin(char *s1, char *s2)
 	return (str);
 }
 
-char	*ft_strchr(char *s, int c)
+char	*ft_strchr(const char *str, int c)
 {
-	int	i;
+	char	*s;
+	int		i;
 
 	i = 0;
+	s = (char *)str;
 	if (!s)
-		return (0);
-	if (c == '\0')
-		return ((char *)&s[ft_strlen(s)]);
-	while (s[i] != '\0')
+		return (NULL);
+	while (s[i])
 	{
 		if (s[i] == (char)c)
-			return ((char *)&s[i]);
+			return (&s[i]);
 		i++;
 	}
-	return (0);
+	if (c == '\0')
+		return (&s[i]);
+	return (NULL);
 }
